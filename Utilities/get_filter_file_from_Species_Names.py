59307ce9741a6527dd6efef696e4ba33f76bb2ee
Ashish Jain <jain.ashishjain1@gmail.com>
2015-10-23 16:43:48 -0500

    Fix for the utility File.

--- Utilities/get_filter_file_from_Species_Names.py
+++ Utilities/get_filter_file_from_Species_Names.py
@@ -5,15 +5,14 @@ import sys
 from Bio import SeqIO
 import re
 
-def printAccessionNumbersFromName(filePath,dbFolderPath):
+def printAccessionNumbersFromName(filePath,dbFolderPath,outFolder):
     file = open(filePath,"r")
-    o = open("accession.txt","w")
+    o = open(outFolder+"/filter.txt","w")
     org = []
     accessions = []
     for line in file:
         temp = '_'.join(line.split("_")[:2])
         org.append(temp.strip())
-    print org
     for dir in util.return_recursive_dir_files(dbFolderPath):
         dirSplit = dir.split("/")
         organism = ""
@@ -25,7 +24,6 @@ def printAccessionNumbersFromName(filePath,dbFolderPath):
             organism_tmp = seq_record.annotations['organism'].replace(' ', '_')
             organism_tmp_1 = re.sub('[\[\]]', "", organism_tmp)
             organism = '_'.join(organism_tmp_1.split('_')[:2])#+"_"+accession
-        print organism
         try:
             if(org.index(organism) >= 0):
                 accessions.append(accession)
@@ -38,9 +36,10 @@ def printAccessionNumbersFromName(filePath,dbFolderPath):
     file.close()
     
 def main():
-    filePath = sys.argv[0];
-    dbPath = sys.argv[1];
-    printAccessionNumbersFromName(filePath,dbPath)
+    filePath = sys.argv[1]
+    dbPath = sys.argv[2]
+    outFolder = sys.argv[3]
+    printAccessionNumbersFromName(filePath,dbPath,outFolder)
         
 if __name__ == "__main__":
     main()
