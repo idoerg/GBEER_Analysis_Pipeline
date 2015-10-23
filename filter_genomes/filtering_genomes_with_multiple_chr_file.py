e7dbd6e3820b730219f49eb8bc78de6820ba3abd
Ashish Jain <jain.ashishjain1@gmail.com>
2015-10-23 13:28:32 -0500

    Fixed the code for removing plasmids and genomes with multiple chromosomes.

--- filter_genomes/filtering_genomes_with_multiple_chr_file.py
+++ filter_genomes/filtering_genomes_with_multiple_chr_file.py
@@ -1,45 +1,45 @@
 #!/usr/bin/env python
 
-import numpy as np
 import os,sys
-from Bio import SeqIO,SeqFeature
-from Bio.SeqRecord import SeqRecord
-from Bio import Phylo
-import re
+from Bio import SeqIO
 CURRENT_DIR = os.path.dirname(os.path.abspath('../Utilities/Utilities.py'))
 sys.path.append(os.path.dirname(CURRENT_DIR))
 import Utilities.Utilities as util
 import shutil
-#import sys
 
 def filterGenomes(rootDir):
-    filter_genomes_list = []
+    filter_genomes_list = {}
     for file in util.return_recursive_dir_files(rootDir):
-	    print file
-            files = util.return_recursive_files(file)
-            fileName = ""
-            if len(files) > 1:
-                for f in files:
-                    seq_record = SeqIO.parse(open(f), "genbank").next()
-                    accession = seq_record.annotations['accessions'][0]
-                    definition = seq_record.description;
-                    if ("plasmid" not in definition):
-                        if fileName != "":
-                            filter_genomes_list.append(os.path.dirname(os.path.abspath(f)))
-                        fileName = accession+".gbk"
-                for f in files:
-                    if os.path.basename(f) != fileName:
-                        os.remove(f)
+        #print file
+        files = util.return_recursive_files(file)
+        fileName = ""
+        if len(files) > 1:
+            for f in files:
+                seq_record = SeqIO.parse(open(f), "genbank").next()
+                accession = seq_record.annotations['accessions'][0]
+                definition = seq_record.description;
+                if ("plasmid" not in definition):
+                    if fileName != "":
+                        #print os.path.dirname(os.path.abspath(f))
+                        filter_genomes_list.update({os.path.dirname(os.path.abspath(f)):""})
+                    fileName = accession+".gbk"
+            for f in files:
+                if os.path.basename(f) != fileName:
+                    #print f
+                    os.remove(f)
     ##Deleting all the genomes having multiple chromosomes files
-    for folder in filter_genomes_list:
+    #print len(filter_genomes_list)
+    for folder in filter_genomes_list.iterkeys():
         shutil.rmtree(folder)
     
         
 def main():
-    rootDir = sys.argv[0];
+    rootDir = sys.argv[1];
+    #print rootDir
+    #rootDir = "/home/jain/test_genomes_folder"
     filterGenomes(rootDir)
 
 if __name__ == "__main__":
-   # CURRENT_DIR = os.path.dirname(os.path.abspath('../Utilities/Utilities.py'))
-   # sys.path.append(os.path.dirname(CURRENT_DIR))
+    # CURRENT_DIR = os.path.dirname(os.path.abspath('../Utilities/Utilities.py'))
+    # sys.path.append(os.path.dirname(CURRENT_DIR))
     main()
